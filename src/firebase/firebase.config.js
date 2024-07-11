a5d1b1fbe20350ee62b9ee1f5ba825bc501c808f
Arittro7 <arittro007@gmail.com>
2024-07-11 08:49:31 +0600

    done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,15 +2,14 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBOIDxZjIQRZfqjMHoWacPo8qnTvVwAjxc",
-  authDomain: "react-dragon-news-auth-69efe.firebaseapp.com",
-  projectId: "react-dragon-news-auth-69efe",
-  storageBucket: "react-dragon-news-auth-69efe.appspot.com",
-  messagingSenderId: "5694724896",
-  appId: "1:5694724896:web:2b0487975cb5939eff9545"
+  apiKey: "import.meta.env.VITE_APIKEY",
+  authDomain: "import.meta.env.VITE_AUTHDOMAIN",
+  projectId: "import.meta.env.VITE_PROJECTID",
+  storageBucket: "import.meta.env.VITE_STORAGEBUCKET",
+  messagingSenderId: "import.meta.env.VITE_MESSAGINGSENDERID",
+  appId: "import.meta.env.VITE_APPID"
 };
 
 // Initialize Firebase
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import routes from './Routes/Routes.jsx'
-import AuthProviders from './Providers/AuthProviders.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import routes from "./Routes/Routes.jsx";
+import AuthProviders from "./Providers/AuthProviders.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProviders><RouterProvider router={routes}></RouterProvider></AuthProviders>
-  </React.StrictMode>,
-)
+    <AuthProviders>
+      <RouterProvider router={routes}></RouterProvider>
+    </AuthProviders>
+  </React.StrictMode>
+);
